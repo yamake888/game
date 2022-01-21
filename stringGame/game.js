ddd1a8b780c99e25f333aa41601cbf1a351a30b8
yamake888 <50048294+yamake888@users.noreply.github.com>
2022-01-21 22:48:27 +0900

    Update game.js

--- stringGame/game.js
+++ stringGame/game.js
@@ -4,8 +4,9 @@ var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height/2;
 
-var dx = 2;
-var dy = -2;
+var v = 2;
+var dx = v;
+var dy = -v;
 var r = 10;
 function draw(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
@@ -16,9 +17,9 @@ function draw(){
     ctx.closePath();
     x+=dx;
     y+=dy;
-    if(x<=r)dx=2;
-    else if(x+r>=canvas.width)dx=-2;
-    if(y<=r)dy=2;
-    else if(y+r>=canvas.height)dy=-2;
+    if(x<=r)dx=v;
+    else if(x+r>=canvas.width)dx=-v;
+    if(y<=r)dy=v;
+    else if(y+r>=canvas.height)dy=-v;
 }
 setInterval(draw,10);
